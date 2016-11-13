b7389040850312a6840221e5a284203dcad0a34f
Jan Bajena <bajena3@gmail.com>
2016-11-13 14:53:36 +0100

    Remove needless code

--- ItoFuzzyLogicInference/ItoFuzzyLogicInference.Tests/InferenceFactoryTest.cs
+++ ItoFuzzyLogicInference/ItoFuzzyLogicInference.Tests/InferenceFactoryTest.cs
@@ -5,18 +5,4 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ItoFuzzyLogicInference.Tests
 {
-    [TestClass]
-    public class InferenceFactoryTest
-    {
-        [TestMethod]
-        public void XmlLoadIntegrationTest()
-        {
-            var factory = new InferenceFactory("Fixtures/simple_inference.xml");
-            factory.LoadData();
-            var inference = new FuzzyInference(factory.FuzzyRules);
-            Assert.AreEqual(2, inference.Rules.Count());
-            Assert.AreEqual(2, factory.LinguisticVariables.Count());
-            Assert.AreSame(inference.Rules.First().Conditions.First().MembershipFunction.LinguisticVariable, factory.LinguisticVariables.First());
-        }
-    }
 }
